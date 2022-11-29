9b2362888b538213a4efae08c32eec93c96fbd52
Kafter <115289409+inf2021090@users.noreply.github.com>
2022-11-29 20:07:06 +0200

    Delete convert.sh

--- statistic/Labs/convert.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-for filename in Labs/lab*.md; do
-  [ -e "$filename"] || continue
-  pandoc --lua-filter=im.lua --to markdown | pandoc --top-level-division=chapter --to latex > latex/"$(basename "4filename" .txt)".tex"
